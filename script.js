2c6a74302ad46046f54f56b43de414692c841141
Harsh Srivastava <harshsrivastavasanu2003@gmail.com>
2024-08-24 16:41:31 +0530

    added all the files

---
+++ script.js
@@ -0,0 +1,81 @@
+const colorpicker = document.getElementById("colorpicker");
+const  canvascolor = document.getElementById("canvascolor");
+const canvas = document.getElementById("mycanvas");
+const clearbutton = document.getElementById("clearButton");
+const savebutton = document.getElementById("saveButton");
+const retrievebutton = document.getElementById("retrieveButton");
+const fontsize = document.getElementById("fontsize");
+
+const ctx = canvas.getContext('2d');
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+
+
+colorpicker.addEventListener('change',(e)=>{
+    ctx.strokeStyle = e.target.value;
+    ctx.fillStyle= e.target.value;
+});
+
+canvas.addEventListener('mousedown',(e)=>{
+    isDrawing = true;
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+});
+
+canvas.addEventListener('mousemove',(e)=>{
+   if(isDrawing){
+    ctx.beginPath();
+    ctx.moveTo(lastX,lastY);
+    ctx.lineTo(e.offsetX,e.offsetY);
+    ctx.stroke();
+
+    lastX = e.offsetX;
+    lastY= e.offsetY;
+
+   }
+});
+
+canvas.addEventListener('mouseup',()=>{
+    isDrawing = false;
+});
+
+canvascolor.addEventListener('change',(e)=>{
+    ctx.fillStyle = e.target.value;
+    ctx.fillRect(0,0,800,500);
+});
+
+fontsize.addEventListener('change',(e)=>{
+    ctx.lineWidth = e.target.value;
+
+});
+
+clearButton.addEventListener('click',()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+});
+
+saveButton.addEventListener('click',()=>{
+    localStorage.setItem('canvasContents',canvas.toDataURL());
+
+    let link = document.createElement('a');
+
+    link.download = 'my-canvas.png';
+
+    link.href = canvas.toDataURL();
+
+    link.click();
+});
+
+retrieveButton.addEventListener('click',()=>{
+
+    let savedCanvas = localStorage.getItem('canvasContents');
+
+    if(savedCanvas){
+        let img = new Image();
+        img.src = savedCanvas;
+        ctx.drawImage(img,0,0)
+
+    }
+})
